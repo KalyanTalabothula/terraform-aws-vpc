c5bbbc0a6405f840272b7fa1a188c6ef857d9652
kalyantalbothula <kalyan6166@gmail.com>
2025-08-14 16:07:38 +1000

    updating

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,23 @@
 # 🔍 terraform aws vpc
 
-# taking 
+# Naming convention
+
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block 
   instance_tenancy = "default"
   enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+# Internet Gate way (IGW)
+resource "aws_internet_gateway" "main" {  # association with VPC
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
     local.common_tags,
