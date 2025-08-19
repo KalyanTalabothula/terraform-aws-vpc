543f420e9fe51879468207ea27e61285dffb2a50
kalyantalbothula <kalyan6166@gmail.com>
2025-08-19 22:31:47 +1000

    terraform

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,17 @@
 output "vpc_id" {
     value = aws_vpc.main.id   #for vpc Id 
 }
+
+output "public_subnet_ids" {      # <--- list kada 
+    value = aws_subnet.public[*].id
+}
+
+# value = aws_subnet.public[*].id means, terraform-aws-vpc (module creation team) lo vpc.tf lo line number : 40 lo chuste you will understand, [ of star ] id ani este everything ani ardham yenni public subnets unte anni id's output evvu ani ardham. 
+
+output "private_subnet_ids" {      # <--- list kada 
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {      # <--- list kada 
+    value = aws_subnet.dasebase[*].id
+}
